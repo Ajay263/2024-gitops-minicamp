498498547098584f1ae05e6fd1a34f71a8651459
Alexio Junior Aaron <97569992+Ajay263@users.noreply.github.com>
2025-04-10 01:12:43 +0200

    Update full_refresh_dag.py

--- airflow/dags/full_refresh_dag.py
+++ airflow/dags/full_refresh_dag.py
@@ -53,6 +53,7 @@ default_args = {
     "owner": "airflow",
     'retries': 1,
     'retry_delay': pendulum.duration(minutes=5),
+    "on_failure_callback": slack_notify.send_failure_alert ,
     'params': {
         'start_time': Param(None, type=["null", "string"], format="date", description="Start date for run", title="Start date"),
         'end_time': Param(None, type=["null", "string"], format="date", description="End date for run", title="End date"),
